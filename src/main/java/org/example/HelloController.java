38a8a4577a94fa21fddbe540740792e3e6df9e3c
sivaraj <bsivaraj@gmail.com>
2026-03-01 17:15:12 +0530

    removed unused imports

--- src/main/java/org/example/HelloController.java
+++ src/main/java/org/example/HelloController.java
@@ -2,8 +2,6 @@ package org.example;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
